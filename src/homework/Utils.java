8f0f0cca1ac473436a422160c6330b1759e05831
Anastasiia Potapenko <oberonapa@gmail.como>
2022-08-05 19:22:11 -0400

    "HW 6 - Tasks conditions"

---
+++ src/homework/Utils.java
@@ -0,0 +1,47 @@
+package homework;
+
+public class Utils extends Variables {
+
+    public static void printNewRow() {
+        System.out.println("\n");
+    }
+
+    public static String printLine() {
+        return ANSI_PURPLE + "*".repeat(15) + ANSI_BLACK;
+    }
+
+    public static void printTaskNUmber(int number) {
+        System.out.println(printLine() + "_Task " + ANSI_BLUE + number + "_" + printLine());
+    }
+
+    public static void printSubparagraphNumber(int number, String letter) {
+        System.out.println(printLine() + "_Task " + ANSI_BLUE + number + "_" + letter + printLine());
+    }
+
+    public static void verifyEquals(String expectedResult, String actualResult) {
+        if (expectedResult.equals(actualResult)) {
+            System.out.println(Variables.ANSI_GREEN + "Pass");
+        } else {
+            System.out.println(Variables.ANSI_RED + "Fail");
+        }
+        printNewRow();
+    }
+
+    public static void verifyEquals(int expectedResult, int actualResult) {
+        if (expectedResult == actualResult) {
+            System.out.println(Variables.ANSI_GREEN + "Pass");
+        } else {
+            System.out.println(Variables.ANSI_RED + "Fail");
+        }
+        printNewRow();
+    }
+
+    public static void verifyEquals(double expectedResult, double actualResult) {
+        if (expectedResult == actualResult) {
+            System.out.println(Variables.ANSI_GREEN + "Pass");
+        } else {
+            System.out.println(Variables.ANSI_RED + "Fail");
+        }
+        printNewRow();
+    }
+}
