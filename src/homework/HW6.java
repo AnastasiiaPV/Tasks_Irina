537bfd2f27a9fe6511aba18a7dd29f65a8c64300
Anastasiia Potapenko <oberonapa@gmail.como>
2022-08-08 21:19:17 -0400

    "HW 6 - Refactoring"

--- src/homework/HW6.java
+++ src/homework/HW6.java
@@ -34,7 +34,7 @@ public class HW6 extends Utils {
     /********************************************************************************************************
      * 4. Распечатать последовательность чисел, кратных 7, в промежутке (327, 300)*/
     public static void printNumSequence327_300() {
-        for (int i = 327; i >= 300; i--) {
+        for (int i = 326; i > 300; i--) {
             if (i % 7 == 0) {
                 System.out.print(i + "\t");
             }
@@ -56,7 +56,7 @@ public class HW6 extends Utils {
     /********************************************************************************************************
      * 6. Распечатать последовательность четных чисел от 215 до 237 включительно*/
     public static void printPosNumSequence_215_237() {
-        for (int i = 215; i <= 238; i++) {
+        for (int i = 215; i < 238; i++) {
             if (i % 2 == 0) {
                 System.out.print(i + "\t");
             }
@@ -85,7 +85,7 @@ public class HW6 extends Utils {
      * заканчивается максимальным значением short. Числа в последовательности должны быть кратны 15001.
      */
     public static void printShortNumSequence() {
-        for (int i = Short.MIN_VALUE; i <= Short.MAX_VALUE; i++) {
+        for (int i = Short.MIN_VALUE; i < Short.MAX_VALUE; i++) {
             if (i % 15001 == 0) {
                 System.out.print(i + "\t");
             }
@@ -118,8 +118,16 @@ public class HW6 extends Utils {
      *     Точка выхода из цикла - число end.
      */
     public static void printTenthsNumSequence(double start, double end, double step) {
-        for (double i = start; i <= end; i += step) {
-            System.out.print(i + "\t");
+        if (start < end && step > 0) {
+            for (start = start; start < end; start += step) {
+                System.out.print(start + "\t");
+            }
+        } else if (start > end && step < 0) {
+            for (start = start; start > end; start += step) {
+                System.out.print(start + "\t");
+            }
+        } else {
+            System.out.println("Invalide data");
         }
         printNewRow();
     }
@@ -142,15 +150,14 @@ public class HW6 extends Utils {
      * 12. Напишите метод, который принимает целое число n, и выводит все степени числа 2 от 1 до n включительно*/
     public static void printNumPowSequence(int n) {
         for (int i = 1; i <= n; i++) {
-            System.out.print((int) (Math.pow(2, i)) + " ");
+            System.out.print(String.format("%2.0f", Math.pow(2, i)) + " ");
         }
     }
 
 
     /********************************************************************************************************
      * 13. Вывести последовательность 012345678900112233445566778899000…  до числа 9999 включительно.*/
-    public static void printNumSequenceTill_9999() {
-
+    public static void printNumSequence13_1() {
         for (int i = 1; i <= 4; i++) {
             int count = i;
             for (int j = 0; j <= 9; j++) {
@@ -163,11 +170,36 @@ public class HW6 extends Utils {
         }
     }
 
+    public static void printNumSequence13_2() {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 0; j <= 9; j++) {
+                for (int k = 1; k <= i; k++) {
+                    System.out.print(j);
+                }
+            }
+        }
+    }
+
+    public static void printNumSequence13_3(int number) {
+        for (int i = 1; i <= number; i++) {
+            System.out.print("0".repeat(i)
+                    + "1".repeat(i)
+                    + "2".repeat(i)
+                    + "3".repeat(i)
+                    + "4".repeat(i)
+                    + "5".repeat(i)
+                    + "6".repeat(i)
+                    + "7".repeat(i)
+                    + "8".repeat(i)
+                    + "9".repeat(i));
+        }
+    }
+
 
     /********************************************************************************************************
      * 14.  Распечатайте последовательность чисел:
      0, 1, -1, 2, -2, 3, -3, 4, -4, 5, -5*/
-    public static void printNumSequence() {
+    public static void printNumSequence_14() {
         int count = 1;
         while (count <= 5) {
             for (int i = 0; i < 5; i++) {
@@ -184,15 +216,39 @@ public class HW6 extends Utils {
         System.out.print(-5);
     }
 
+    public static void printNumSequence_14_2() {
+        for (int i = 0, j = 1; i > -6 || j <= 5; i--, j++) {
+            if (j < 6) {
+                System.out.print(i + ", " + j + ", ");
+            } else {
+                System.out.print(i);
+            }
+        }
+    }
+
+    public static void printNumSequence_14_3() {
+        System.out.print(0);
+        for (int i = 1; i < 6; i++) {
+            System.out.print(", " + i + ", " + (i * -1));
+        }
+    }
+
+    public static void printNumSequence_14_4() {
+        for (int i = 0; i < 6; i++) {
+            System.out.print(i + ", ");
+            if (i >= 1) {
+                System.out.print(i * (-1) + ", ");
+            }
+        }
+    }
+
+
     /********************************************************************************************************
      * 15. Распечатать последовательность чисел:
      0, 3, 5, 6, 9, 10, 12, 15, 18, 20, 21, 24, 25*/
     public static void printNumSequence15(int a) {
-        System.out.print("?".repeat(15));
         for (int i = 0; i <= a; i++) {
-            if (i == a) {
-                System.out.println(i);
-            } else if (i % 2 == 0 || i % 3 == 0 || i % 5 == 0) {
+            if (i % 3 == 0 || i % 5 == 0) {
                 System.out.print(i + ", ");
             }
         }
@@ -224,9 +280,16 @@ public class HW6 extends Utils {
      * 17.  Сгенерируйте и распечатайте последовательность по формуле:
      n + 1 = n + 2*/
     public static void printNumSequence17(int n, int l) {
-        for (int i = 0; i < l; i++) {
+        for (int i = n; i < l; i++) {
             System.out.print(n + " ");
             n += 2;
+        }
+    }
+
+    public static void printNumSequence17_2(int n) {
+        int l = n + 5;
+        for (; n < l; n++) {
+            System.out.println("[" + (n +1) + "] = " + (n +2) + " ");
         }
     }
 
@@ -238,9 +301,17 @@ public class HW6 extends Utils {
      *      Длина последовательности  l.
      */
     public static void printNumSequence18(int n, int l) {
-        for (int i = 0; i < l; i++) {
+        for (int i = n+1; i < l; i++) {
             System.out.print(n + " ");
             n *= 2;
+        }
+    }
+
+    public static void printNumSequence18_2(int n, int l) {
+        System.out.println("[" + (n) + "] = " + (n) + " ");
+
+        for (n = n; n < l; n++) {
+            System.out.println("[" + (n + 1) + "] = " + (n * 2) + " ");
         }
     }
 
@@ -310,6 +381,15 @@ public class HW6 extends Utils {
         printTaskNUmber(10);
         printTenthsNumSequence(-15.1, 30.9, 5.5);
 
+        printTaskNUmber(10);
+        printTenthsNumSequence(15.9, 0, -1.45);
+
+        printTaskNUmber(10);
+        printTenthsNumSequence(15.9, 0, 1);
+
+        printTaskNUmber(10);
+        printTenthsNumSequence(-15.9, 0, -1);
+
         printTaskNUmber(11);
         printPositiveNumSequence_0_L(25);
         printNewRow();
@@ -319,11 +399,33 @@ public class HW6 extends Utils {
         printNewRow();
 
         printTaskNUmber(13);
-        printNumSequenceTill_9999();
+        printNumSequence13_1();
+        printNewRow();
+
+        printTaskNUmber(13);
+        printNumSequence13_2();
+        printNewRow();
+
+        printTaskNUmber(13);
+        printNumSequence13_3(5);
+        printNewRow();
 
         printTaskNUmber(14);
-        printNumSequence();
+        printNumSequence_14();
         printNewRow();
+
+        printSubparagraphNumber(14, "2");
+        printNumSequence_14_2();
+        printNewRow();
+
+        printSubparagraphNumber(14, "3");
+        printNumSequence_14_3();
+        printNewRow();
+
+        printSubparagraphNumber(14, "4");
+        printNumSequence_14_4();
+        printNewRow();
+
 
         printTaskNUmber(15);
         printNumSequence15(25);
@@ -333,12 +435,24 @@ public class HW6 extends Utils {
         printNumSequence16(5, 3, 20);
         printNewRow();
 
+        printSubparagraphNumber(16,"2");
+        printNumSequence16_2(5, 3, 20);
+        printNewRow();
+
         printTaskNUmber(17);
         printNumSequence17(5, 20);
         printNewRow();
 
+        printSubparagraphNumber(17, "2");
+        printNumSequence17_2(5);
+        printNewRow();
+
         printTaskNUmber(18);
-        printNumSequence18(3, 20);
+        printNumSequence18(1, 10);
+        printNewRow();
+
+        printSubparagraphNumber(18, "2");
+        printNumSequence18_2(1, 10);
         printNewRow();
 
 
