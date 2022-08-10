360450b39d7a90004778e52f54a2276ce917c8d1
Anastasiia Potapenko <oberonapa@gmail.como>
2022-08-09 23:39:40 -0400

    "HW 7 _ Tasks"

--- src/homework/HW7.java
+++ src/homework/HW7.java
@@ -40,6 +40,10 @@ public class HW7 extends Utils {
     }
 
 
+    public static int[] getCatsAgesInt() {
+        return new int[]{5, 3, 15, 0, 10, 6, 5, 4};
+    }
+
     /**************************************************************************************************************
      * 5. Создать массив isCatRed и заполнить его соответствующими значениями*/
     public static boolean isCatRed(String color) {
@@ -111,16 +115,16 @@ public class HW7 extends Utils {
     /**************************************************************************************************************
      * 8. Распечатать “Отнеси кота на прививку!”, если возраст кота меньше 2 лет*/
     //[1.2, 3.0, 15.1, 0.9, 10.0, 6.6, 5.0, 3.2]
-        public static void doTheShot() {
-            StringBuilder result = new StringBuilder();
+    public static void doTheShot() {
+        StringBuilder result = new StringBuilder();
 
-            for (int i = 0; i < getCatsAges().length; i++) {
-                if (getCatsAges()[i] < 2) {
-                    result.append(getCatsAges()[i]).append(" - Отнеси кота на прививку!").append("\n");
-                }
+        for (int i = 0; i < getCatsAges().length; i++) {
+            if (getCatsAges()[i] < 2) {
+                result.append(getCatsAges()[i]).append(" - Отнеси кота на прививку!").append("\n");
             }
-            System.out.println(result);
         }
+        System.out.println(result);
+    }
 
 
     /**************************************************************************************************************
@@ -129,9 +133,9 @@ public class HW7 extends Utils {
     //[Gray, Black, Yellow, Gray, Red, Pockmarked, Red, Gray]
     //[1.2, 3.0, 15.1, 0.9, 10.0, 6.6, 5.0, 3.2]
     public static void printLastCat() {
-        System.out.println(new StringBuilder().append("Name: ").append(getCatsNames_2()[getCatsNames_2().length-1])
-                .append("\nColor: ").append(getCatsColors()[getCatsColors().length-1])
-                .append("\nAge: ").append(getCatsAges()[getCatsAges().length-1]));
+        System.out.println(new StringBuilder().append("Name: ").append(getCatsNames_2()[getCatsNames_2().length - 1])
+                .append("\nColor: ").append(getCatsColors()[getCatsColors().length - 1])
+                .append("\nAge: ").append(getCatsAges()[getCatsAges().length - 1]));
     }
 
 
@@ -167,84 +171,269 @@ public class HW7 extends Utils {
         System.out.println(result);
     }
 
-            public static void main (String[]args){
-            printTaskNUmber(1);
-            getCatsNames();
-            printNewRow();
 
-            printTaskNUmber(2);
-            System.out.println(Arrays.toString(getCatsNames_2()));
-            printNewRow();
-
-            printTaskNUmber(3);
-            System.out.println(Arrays.toString(getCatsColors()));
-            printNewRow();
-
-            printTaskNUmber(4);
-            System.out.println(Arrays.toString(getCatsAges()));
-            printNewRow();
-
-            printTaskNUmber(5);
-            System.out.println(isCatRed("Red"));
-            System.out.println(isCatRed("Black"));
-            printNewRow();
-
-            printTaskNUmber(6);
-            printName();
-
-            printTaskNUmber(7);
-            feedGrayCats();
-
-            printTaskNUmber(8);
-            doTheShot();
-
-            printTaskNUmber(9);
-            printLastCat();
-
-            printTaskNUmber(10);
-            printCatsNames_10();
-
-            printTaskNUmber(11);
-            feedCat_11();
-
+    /**************************************************************************************************************
+     * 12. Распечатать средний возраст котов из массива catsAges*/
+    public static void printAvgAge() {
+        double avg = 0;
+        for (int i = 0; i < getCatsAges().length; i++) {
+            avg += getCatsAges()[i];
         }
+        System.out.println(avg/getCatsAges().length-1);
+    }
 
 
+    /**************************************************************************************************************
+     * 13. Распечатать возраст самого молодого кота*/
+    public static void printYoungestCat() {
+        double min = Double.MAX_VALUE;
+        for (int i = 0; i < getCatsAges().length; i++) {
+            if (getCatsAges()[i] < min){
+                min = getCatsAges()[i];
+            }
+        }
+        System.out.println(min);
+    }
+
+    /**************************************************************************************************************
+     * 14. Распечатать возраст самого старого кота*/
+    public static void printOldestCat() {
+        double max = Double.MIN_VALUE;
+        for (int i = 0; i < getCatsAges().length; i++) {
+            if (getCatsAges()[i] > max){
+                max = getCatsAges()[i];
+            }
+        }
+        System.out.println(max);
+    }
+
+    /**************************************************************************************************************
+     * 15. Распечатать количество серых котов*/
+    public static void printNumGrayCats() {
+        int count = 0;
+        for (int i = 0; i < getCatsColors().length; i++) {
+            if (getCatsColors()[i].equals("Gray")){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 
 
-        /**************************************************************************************************************
-         * 12. Распечатать средний возраст котов из массива catsAges*/
-        /**************************************************************************************************************
-         * 13. Распечатать возраст самого молодого кота*/
-        /**************************************************************************************************************
-         * 14. Распечатать возраст самого старого кота*/
-        /**************************************************************************************************************
-         * 15. Распечатать количество серых котов*/
-        /**************************************************************************************************************
-         * 16. Распечатать имя кота, если кот находится в коробке с четным индексом и его возраст не больше 3 лет*/
-        /**************************************************************************************************************
-         * 17. Создать массив четных положительных чисел, значения которых не больше 10.
-         (заполняем значения с помощью цикла for)*/
-        /**************************************************************************************************************
-         * 18. Написать метод, который принимает на вход массив int, и возвращает среднее значение.
-         Проверить работу метода тестом, если параметр - массив catsAges*/
-        /**************************************************************************************************************
-         * 19. Создать массив нечетных отрицательных чисел в промежутке от -1000 до -900*/
-        /**************************************************************************************************************
-         * 20. Создать массив из 10 случайных положительных целых чисел*/
+    /**************************************************************************************************************
+     * 16. Распечатать имя кота, если кот находится в коробке с четным индексом и его возраст не больше 3 лет*/
+    //[0,      1,      2,           3,            4,      5,     6,      7]
+    //[Ruddy,  Черныш, Light Ruddy, Middle Ruddy, Рыжик,  Grey,  Yellow, Dark Gray]
+    //[1.2,    3.0,    15.1,        0.9,          10.0,   6.6,   5.0,    3.2]
+    public static void printCat_16() {
+        for (int i = 0; i < getCatsNames_2().length; i++) {
+            if (i % 2 == 0 && getCatsAges()[i] <= 3) {
+                System.out.println(getCatsNames_2()[i]);
+            }
+        }
+    }
 
 
-        /**************************************************************************************************************
-         * 21. Создать метод, который принимает на вход массив int,  и возвращает минимальное значение,
-         максимальное значение и среднее значение всех чисел массива. Проверить работу метода на массиве из задания 20.*/
-        /**************************************************************************************************************
-         * 22. Создать массив четных чисел и массив нечетных чисел из элементов массива из задания 20.*/
-        /**************************************************************************************************************
-         * 23. Создать двумерный массив, который состоит из имен, возрастов, цветов котов:
-         Распечатать все данные котов в коробках с четными индексами, используя двумерный массив.*/
-        /**************************************************************************************************************
-         * 24. Создать двумерный массив целых случайных чисел от 1 до 10 размерности 4*8.*/
-        /**************************************************************************************************************
-         * 25. Вывести сумму всех четных чисел массива из задания 24.*/
+    /**************************************************************************************************************
+     * 17. Создать массив четных положительных чисел, значения которых не больше 10.
+     (заполняем значения с помощью цикла for)*/
+    public static int[] getPositiveNum() {
+        int[] result = {0};
+        for (int i = 1; i < 11; i++) {
+            if(i % 2 == 0) {
+                result = Arrays.copyOf(result, result.length+1);
+                result[result.length-1] = i;
+            }
+        }
+        System.out.println(Arrays.toString(result));
+        return result;
+    }
+
+
+    /**************************************************************************************************************
+     * 18. Написать метод, который принимает на вход массив int, и возвращает среднее значение.
+     Проверить работу метода тестом, если параметр - массив catsAges*/
+    public static double getAverage(int[] arr) {
+        double avg = 0;
+        for (int j : arr) {
+            avg += j;
+        }
+        return avg/arr.length;
+    }
+
+    /**************************************************************************************************************
+     * 19. Создать массив нечетных отрицательных чисел в промежутке от -1000 до -900*/
+    public static int[] getNegativeNum() {
+        int[] result = {};
+        for (int i = -1000; i < -900; i++) {
+            if(i % 2 != 0) {
+                result = Arrays.copyOf(result, result.length+1);
+                result[result.length-1] = i;
+            }
+        }
+        System.out.println(Arrays.toString(result));
+        return result;
+    }
+
+
+    /**************************************************************************************************************
+     * 20. Создать массив из 10 случайных положительных целых чисел*/
+    public static int[] getRandomNumArr() {
+        int[] arr = {};
+        int i = 0;
+        while(i < 10) {
+            int num = Math.abs((int)Math.round(Math.random() * 10));
+            arr = Arrays.copyOf(arr, arr.length + 1);
+            arr[arr.length-1] = num;
+            i++;
+        }
+        System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+
+
+    /**************************************************************************************************************
+     * 21. Создать метод, который принимает на вход массив int,  и возвращает минимальное значение,
+     максимальное значение и среднее значение всех чисел массива. Проверить работу метода на массиве из задания 20.*/
+    public static void getMinMaxAvg(int[] arr) {
+        double avg = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] < min){
+                min = arr[i];
+            }
+            if (arr[i] > max){
+                max = arr[i];
+            }
+            avg += arr[i];
+        }
+        System.out.println("Average: " + avg/arr.length + ";\nMin: " + min + ";\nMax: " + max + ";");
+    }
+
+    /**************************************************************************************************************
+     * 22. Создать массив четных чисел и массив нечетных чисел из элементов массива из задания 20.*/
+    public static void getEvenOddNum(int[] arr) {
+        int[] odd = {};
+        int[] even = {};
+
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] % 2 != 0) {
+                odd = Arrays.copyOf(odd, odd.length + 1);
+                odd[odd.length - 1] = arr[i];
+            } else {
+                even = Arrays.copyOf(even, even.length + 1);
+                even[even.length - 1] = arr[i];
+            }
+        }
+        System.out.println(Arrays.toString(even) + "\n" + Arrays.toString(odd));
+    }
+
+    public static void getEvenOddNum_2(int[] arr) {
+        int[] odd = {};
+        int[] even = {};
+
+        for (int j : arr) {
+            if (j % 2 != 0) {
+                odd = Arrays.copyOf(odd, odd.length + 1);
+                odd[odd.length - 1] = j;
+            } else {
+                even = Arrays.copyOf(even, even.length + 1);
+                even[even.length - 1] = j;
+            }
+        }
+        System.out.println(Arrays.toString(even) + "\n" + Arrays.toString(odd));
+    }
+
+
+    public static void main(String[] args) {
+        printTaskNUmber(1);
+        getCatsNames();
+        printNewRow();
+
+        printTaskNUmber(2);
+        System.out.println(Arrays.toString(getCatsNames_2()));
+        printNewRow();
+
+        printTaskNUmber(3);
+        System.out.println(Arrays.toString(getCatsColors()));
+        printNewRow();
+
+        printTaskNUmber(4);
+        System.out.println(Arrays.toString(getCatsAges()));
+        printNewRow();
+
+        printTaskNUmber(5);
+        System.out.println(isCatRed("Red"));
+        System.out.println(isCatRed("Black"));
+        printNewRow();
+
+        printTaskNUmber(6);
+        printName();
+
+        printTaskNUmber(7);
+        feedGrayCats();
+
+        printTaskNUmber(8);
+        doTheShot();
+
+        printTaskNUmber(9);
+        printLastCat();
+
+        printTaskNUmber(10);
+        printCatsNames_10();
+
+        printTaskNUmber(11);
+        feedCat_11();
+
+        printTaskNUmber(12);
+        printAvgAge();
+
+        printTaskNUmber(13);
+        printYoungestCat();
+
+        printTaskNUmber(14);
+        printOldestCat();
+
+        printTaskNUmber(15);
+        printNumGrayCats();
+
+        printTaskNUmber(16);
+        printCat_16();
+
+        printTaskNUmber(17);
+        getPositiveNum();
+
+        printTaskNUmber(18);
+        System.out.println(getAverage(getCatsAgesInt()));
+        verifyEquals(getAverage(getCatsAgesInt()), 6);
+
+        printTaskNUmber(19);
+        getNegativeNum();
+        printNewRow();
+
+        printTaskNUmber(20);
+        getRandomNumArr();
+        printNewRow();
+
+        printTaskNUmber(21);
+        getMinMaxAvg(getRandomNumArr());
+        printNewRow();
+
+        printTaskNUmber(22);
+        getEvenOddNum(getRandomNumArr());
+        printNewRow();
 
     }
+
+
+    /**************************************************************************************************************
+     * 23. Создать двумерный массив, который состоит из имен, возрастов, цветов котов:
+     Распечатать все данные котов в коробках с четными индексами, используя двумерный массив.*/
+    /**************************************************************************************************************
+     * 24. Создать двумерный массив целых случайных чисел от 1 до 10 размерности 4*8.*/
+    /**************************************************************************************************************
+     * 25. Вывести сумму всех четных чисел массива из задания 24.*/
+
+}
